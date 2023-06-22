a89837e6599bd6de9b328d5ced5413bfc37bebe8
dbowton <80086095+dbowton@users.noreply.github.com>
2023-06-21 20:25:06 -0500

    VR UI Update
    Potential  addition of raycast for menu select

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -25,16 +25,19 @@ public class UIManager : MonoBehaviour
 
 	private void Update()
 	{
+		//	manual joystick scroll toggle 
 		if (!released)
 		{
 			ControllerManager.rightInput.GetControllerPressed(VRButton.primaryButton, out bool startedPressed);
 			released = !startedPressed;
 		}
+		//	manual primary button select
 		else if (ControllerManager.rightInput.GetControllerPressed(VRButton.primaryButton, out bool pressed) && pressed)
 		{
 			images[index].GetComponent<Image>().material = clicked_mat;
 			images[index].GetComponent<EventHolder>().action.Invoke();
 		}
+		//	manual joystick scroll
 		else if (ControllerManager.rightInput.GetControllerPressed(VRButton.primary2DAxis, out Vector2 dir))
 		{
 			if (dir.y > 0.7f || dir.y < -0.7f)
@@ -62,6 +65,48 @@ public class UIManager : MonoBehaviour
 			else
 			{
 				ready = true;
+			}
+		}
+
+		//	raycast from input controllers
+		//	if hit selectable object set index
+		//	check triggers of selected controllers
+
+		//	check Left
+		Ray leftRay = new Ray(ControllerManager.leftController.transform.position, ControllerManager.leftController.transform.forward);
+		if (Physics.Raycast(leftRay, out RaycastHit leftHitinfo))
+		{
+			if(images.Contains(leftHitinfo.collider.gameObject))
+			{
+				images[index].GetComponent<Image>().material = default_mat;
+				for (index = 0; images[index] != leftHitinfo.collider.gameObject; index++) ;
+
+				images[index].GetComponent<Image>().material = selected_mat;
+
+				if(ControllerManager.leftInput.GetControllerPressed(VRButton.triggerButton, out bool Leftpressed) && Leftpressed)
+				{
+					images[index].GetComponent<Image>().material = clicked_mat;
+					images[index].GetComponent<EventHolder>().action.Invoke();
+				}
+			}
+		}
+
+		//	check right
+		Ray rightRay = new Ray(ControllerManager.rightController.transform.position, ControllerManager.rightController.transform.forward);
+		if (Physics.Raycast(rightRay, out RaycastHit rightHitinfo))
+		{
+			if (images.Contains(rightHitinfo.collider.gameObject))
+			{
+				images[index].GetComponent<Image>().material = default_mat;
+				for (index = 0; images[index] != rightHitinfo.collider.gameObject; index++) ;
+
+				images[index].GetComponent<Image>().material = selected_mat;
+
+				if (ControllerManager.leftInput.GetControllerPressed(VRButton.triggerButton, out bool rightPressed) && rightPressed)
+				{
+					images[index].GetComponent<Image>().material = clicked_mat;
+					images[index].GetComponent<EventHolder>().action.Invoke();
+				}
 			}
 		}
 	}
